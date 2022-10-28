ed04a61dce5a9c319612ab3c668fbb83ba281f80
Ibai Heras <98816018+iByN8@users.noreply.github.com>
2022-10-28 16:50:42 +0200

    😊

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
-let comp = 'competitons.json'
+
+let comp = await fetch('../competitions.json').then(r=>r.json()).then(r => r)
 
 function start(){
 
-
-
+alert(comp.competitions[7])
 
 
 
 
 
 }
-window.onload() = start()
+window.onload() = start
