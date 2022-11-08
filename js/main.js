754e9f2a8d8bb2d296c764ef1dac8ae56e047db5
iByN8 <ibaiheras2002@gmail.com>
2022-11-08 23:30:26 +0100

    Milestone2

--- js/main.js
+++ js/main.js
@@ -4,14 +4,13 @@ import { getAge } from "./rows.js";
 
 function differenceInDays(date1) {
     const date = new Date()
-    let fechaInicio = date1.getTime();
-    let fechaFin = date.getTime();
-    
-    let dif = Math.abs(fechaFin - fechaInicio);
+  let fechaInicio = date1.getTime();
+  let fechaFin = date.getTime();
+  
+  let dif = Math.abs(fechaFin - fechaInicio);
 
-    //return (dif/(1000*60*60*24));
-    return 15;
-
+  //return (dif/(1000*60*60*24));
+  return 15;
 }
 
 let difference_In_Days = differenceInDays(new Date("08-18-2022"));
@@ -24,14 +23,14 @@ window.onload = function () {
 };
 
 let game = {
-  guesses: [],  
+  guesses: [],
   solution: {},
   players: [],
   leagues: []
 };
 
- function getSolution(players, solutionArray, difference_In_Days) {
-    let solPlayer = solutionArray[difference_In_Days-1];
+function getSolution(players, solutionArray, difference_In_Days) {
+  let solPlayer = solutionArray[difference_In_Days-1];
     console.log(solPlayer)
     let player = players.filter(item => item.id == solPlayer.id);
     return player[0];
@@ -53,73 +52,15 @@ Promise.all([fetchJSON("fullplayers"), fetchJSON("solution")]).then(
     ).src = `https://playfootball.games/media/players/${
       game.solution.id % 32
     }/${game.solution.id}.png`;
-  
-  }
 
+
+      // YOUR CODE HERE
+    let addRow = setupRows( /* THIS NEEDS A PARAMETER */ );
+    // get myInput object...
+      // when the user types a number an press the Enter key:
+        addRow( /* the ID of the player, where is it? */);
+    //  
+
+
+  }
 );
-
-
-function check(theKey, theValue) {
-
-  switch (theKey) {
-    /*case theKey === ("nationality" || "leagueId" || "teamId" || "position"):
-      if (game.solution[theKey] == theValue){
-        return true;
-
-      }else{
-        return false;
-
-     }
-    break;
-    */
-    case theKey === "birthdate":
-
-      if (getAge(game.solution[theKey]) == getAge(theValue)){
-          return true;
-
-      }else if (getAge(game.solution[theKey]) >= getAge(theValue)){
-          return "lower";
-
-      }else{
-          return "higher"
-
-      }
-        
-    break;
-
-    case theKey === "number":
-
-      if (game.solution[theKey] == theValue){
-        return true;
-
-      }else if (game.solution[theKey] >= theValue){
-        return "lower";
-
-      }else{
-        return "higher"
-
-      }
-
-
-    break;
-
-    default:
-
-      if (game.solution[theKey] == theValue){
-        return true;
-
-      }else{
-        return false;
-
-      }    
-      break;
-  }
-
-}
-
-
-function getPlayer(playerId){
-
-  return game.players.filter(item => item.id == playerId)[0]
-
-}
