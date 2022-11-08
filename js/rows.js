f1fb7cfe914284543507fe32634119428cf0eba6
Eneko <elarruscain003@ikasle.ehu.eus>
2022-11-08 20:54:16 +0100

    cambio

---
+++ js/rows.js
@@ -0,0 +1,17 @@
+function getAge(dateString){
+    var edad = 0;
+    var arraySeparado = dateString.split("-");
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(mm<arraySeparado[0]){
+        edad=edad+1;
+    }else if(mm=arraySeparado[0] && dd<arraySeparado[1]){
+        edad=edad+1;
+    }
+
+    edad=edad+(yyyy-arraySeparado[2]);
+    return edad;
+}
