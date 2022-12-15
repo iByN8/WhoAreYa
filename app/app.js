4f868668bbf396cd64b33a3710b3d3125b9db2d4
iByN8 <ibaiheras2002@gmail.com>
2022-12-15 22:44:17 +0100

    a

--- app/app.js
+++ app/app.js
@@ -19,6 +19,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// MongoDB
+const mongojs = require('mongojs')
+const db = mongojs('mongodb://127.0.0.1:27017/mongosh', ['players'])
+
 app.use('/', indexRouter);
 app.use('/api/players', usersRouter);
 
