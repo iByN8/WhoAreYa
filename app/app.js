e147ba63bf4fb9580be1f3f231262d7bec04eee9
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2022-12-15 21:30:35 +0100

    a

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-const playersRouter = require('./routes/players');
+const usersRouter = require('./routes/players');
 
 var app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/players', playersRouter);
+app.use('/api/players', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
