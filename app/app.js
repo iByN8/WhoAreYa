0a3841911763010f40660370e5c2aa7011d8dd92
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2022-12-15 21:33:10 +0100

    a

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 const usersRouter = require('./routes/players');
 
-const PORT = 3000;
+
 
 var app = express();
 
@@ -40,7 +40,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);})
+
 
 module.exports = app;
