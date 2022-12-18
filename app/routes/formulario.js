7ec64a3c4c2dd80ac351618e86ac906db9eec88d
Joanes1 <98975037+Joanes1@users.noreply.github.com>
2022-12-19 00:32:03 +0100

    a

--- app/routes/formulario.js
+++ app/routes/formulario.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/formulario', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('formulario');
 });
 
-router.post('/formulario',function(req, res, next) {
+router.post('/',function(req, res, next) {
   console.log(req.body)
   res.send(req.body)
 });
