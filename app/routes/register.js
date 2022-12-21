8ed824ece605dfb6ecfd969287ab8141ecb9a40d
iByN8 <ibaiheras2002@gmail.com>
2022-12-21 19:51:13 +0100

    Update register.js

--- app/routes/register.js
+++ app/routes/register.js
@@ -5,9 +5,6 @@ var router = express.Router();
 const mongojs = require('mongojs')
 const db = mongojs('mongodb://127.0.0.1:27017/footballdata', ['users'])
 
-db.users.insert({name: "Joanes", subname: "Arriola", email: "joanes@email.com", password: "1234", rol: "Admin"})
-db.users.insert({name: "Eneko", subname: "Larruskain", email: "eneko@email.com", password: "1234", rol: "User"})
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { error: '' });
